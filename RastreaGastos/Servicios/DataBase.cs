715fe2e59e37c7d1c2d1cb608b9ce5026b67a5de
Bryan Romero <bryanromerof26@gmail.com>
2026-06-26 18:37:03 -0600

    Añadida clase DataBase para gestión de SQLite
    Se ha añadido la clase `DataBase` en el espacio de nombres
    `RastreaGastos.Servicios` para gestionar la conexión y las
    operaciones con una base de datos SQLite.
    
    - Se añadieron los campos `_databasePath` y `_connectioString`
      para definir la ruta y la cadena de conexión.
    - Se implementó un constructor para inicializar la base de datos.
    - Se añadió el método `IniciarBDAsync` para crear la tabla `Gastos`
      si no existe, con las columnas `ID`, `Descripcion`, `Monto` y `Fecha`.
    - Se añadió el método `LeerGastosAsync` para leer todos los registros
      de la tabla `Gastos` y devolverlos como una lista de objetos `Gastos`.
    - Se añadió la firma del método `AgregarGastoAsync`, aunque su
      implementación aún está pendiente.

--- RastreaGastos/Servicios/DataBase.cs
+++ RastreaGastos/Servicios/DataBase.cs
@@ -10,5 +10,64 @@ namespace RastreaGastos.Servicios
 {
     public class DataBase
     {
+        private readonly string _databasePath;
+        private readonly string _connectioString;
+
+        public DataBase()
+        {
+            _databasePath = Path.Combine(FileSystem.AppDataDirectory, "DB_Gastos.db3");
+            _connectioString = new SqliteConnectionStringBuilder
+            {
+                DataSource = _databasePath
+            }.ToString();
+        }
+
+        //crear tabla en caso de que no exista
+        public Task IniciarBDAsync()
+        {
+            return Task.Run(() =>
+            {
+                using var connection = new SqliteConnection(_connectioString);
+                connection.Open();
+                var tableCommand = connection.CreateCommand();
+                tableCommand.CommandText =
+                @"CREATE TABLE IF NOT EXISTS Gastos (
+                    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Descripcion VARCHAR(200) NOT NULL,
+                    Monto REAL NOT NULL,
+                    Fecha DATE DEAFULT GETDATE()
+                );";
+                tableCommand.ExecuteNonQuery();
+            });
+        }
+
+        public Task<List<Gastos>> LeerGastosAsync()
+        {
+            return Task.Run(() =>
+            {
+                var gastos = new List<Gastos>();
+                using var connection = new SqliteConnection(_connectioString);
+                connection.Open();
+                var selectCommand = connection.CreateCommand();
+                selectCommand.CommandText = @"SELECT * FROM Gastos";
+                using var reader = selectCommand.ExecuteReader();
+                while(reader.Read())
+                {
+                    gastos.Add(new Gastos
+                    {
+                        ID = reader.GetInt32(0),
+                        Descripcion = reader.GetString(1),
+                        Monto = reader.GetDecimal(2),
+                        Fecha = reader.GetDateTime(3)
+                    });
+                }
+                return gastos;
+            });
+        }
+
+        public Task<int> AgregarGastoAsync(Gastos gastos)
+        {
+
+        }
     }
 }
