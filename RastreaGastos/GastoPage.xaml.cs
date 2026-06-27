7bd1223d3f55465d0b210758292d522af49b7ef1
Bryan Romero <bryanromerof26@gmail.com>
2026-06-26 20:20:21 -0600

    Añadida funcionalidad para gestionar gastos en GastoPage
    Se realizaron los siguientes cambios:
    - En **GastoPage.xaml**, se añadieron campos de entrada para la descripción y el monto del gasto, así como botones para guardar y cancelar.
    - En **GastoPage.xaml.cs**, se actualizó el constructor para recibir un servicio de base de datos y un objeto opcional de tipo `Gastos`.
    - Se añadieron los métodos `OnGuardarClicked` y `OnCancelarClicked` para manejar las acciones de los botones.
    - Se incluyó lógica para validar los datos ingresados y actualizar o agregar gastos en la base de datos.

--- RastreaGastos/GastoPage.xaml.cs
+++ RastreaGastos/GastoPage.xaml.cs
@@ -1,9 +1,50 @@
+using Microsoft.Maui.Controls;
+using RastreaGastos.Modelos;
+using RastreaGastos.Servicios;
+using System.Threading.Tasks;
+
 namespace RastreaGastos;
 
 public partial class GastoPage : ContentPage
 {
-	public GastoPage()
+	private readonly Gastos? _gasto;
+	private readonly DataBase _dbService;
+	public GastoPage(DataBase dbService, Gastos? gasto = null)
 	{
 		InitializeComponent();
+		_dbService = dbService;
+		_gasto = gasto;
+
+		if (_gasto != null)
+		{
+			entryDesc.Text = _gasto.Descripcion;
+			entryMonto.Text = _gasto.Monto.ToString();
+		}
 	}
+
+    private async void OnGuardarClicked(object sender, EventArgs e)
+    {
+		if (string.IsNullOrWhiteSpace(entryDesc.Text) || string.IsNullOrWhiteSpace(entryMonto.Text) 
+			|| !decimal.TryParse(entryMonto.Text, out decimal monto))
+		{
+			await DisplayAlert("Error", "Por favor, llene todos los campos con informacion valida.", "Aceptar");
+			return;
+		}
+		_gasto!.Descripcion = entryDesc.Text;
+		_gasto!.Monto = monto;
+		if (_gasto.ID == 0)
+		{
+			await _dbService.AgregarGastoAsync(_gasto);
+		}
+		else
+		{
+			await _dbService.ActualizarGastoAsync(_gasto);
+		}
+		await Navigation.PopAsync();
+    }
+
+    private async void OnCancelarClicked(object sender, EventArgs e)
+    {
+		await Navigation.PopAsync();
+    }
 }
